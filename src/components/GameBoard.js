1a1142e25df7182f124f9b55151a19be9da16b5b
Ahmed-Seleem01 <ahmedseleem.c@gmail.com>
2023-03-22 12:37:09 +0200

    Remove console logs

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -14,13 +14,10 @@ const GameBoard = () => {
 
   const getGameBoard = () => coordinatesArr;
 
-  // const coordinatesArr = createGameBoard();
-  // console.log(coordinatesArr)
   function placeShip(ship = {}, coordinates = []) {
-    const x = coordinates[0] - 1;
-    const y = coordinates[1] - 1;
+    const [x, y] = coordinates;
+
     const slicedArr = coordinatesArr[x].slice(y);
-    console.log(slicedArr[0]);
     if (
       slicedArr.length >= ship.length &&
       slicedArr.every((item) => item == null)
@@ -28,7 +25,6 @@ const GameBoard = () => {
       for (let i = 0; i < ship.length; i++) {
         coordinatesArr[x][y + i] = ship.length;
       }
-      console.log(coordinatesArr[x]);
       return true;
     }
     return false;
